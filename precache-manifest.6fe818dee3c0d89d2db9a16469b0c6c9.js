8b1e0367281ba61839489555ea8fc239f78c9a90
Renxiao Mo <mrenxiao@gmail.com>
2018-12-20 20:33:06 +0100

    Updates

---
+++ precache-manifest.6fe818dee3c0d89d2db9a16469b0c6c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d553009909afa6f8ab63",
+    "url": "/rpost-web/static/css/main.0d242a91.chunk.css"
+  },
+  {
+    "revision": "d553009909afa6f8ab63",
+    "url": "/rpost-web/static/js/main.d5530099.chunk.js"
+  },
+  {
+    "revision": "750bbc6bd198681ea0a9",
+    "url": "/rpost-web/static/js/1.750bbc6b.chunk.js"
+  },
+  {
+    "revision": "3f81c327e90f9dcd09b1",
+    "url": "/rpost-web/static/js/runtime~main.3f81c327.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rpost-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7243eae7ebcb7b9282e29c12ef569238",
+    "url": "/rpost-web/index.html"
+  }
+];
